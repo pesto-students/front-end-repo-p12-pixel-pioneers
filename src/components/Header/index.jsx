4c733de81ffc39343eddec2cb0771576c822d4e6
ketan verma <ketan18710@gmail.com>
2024-03-10 09:43:23 +0530

    header fix on login

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { APP_ROUTES, AuthHelpers } from "../../utils";
 import { Button, Popper } from "@mui/base";
@@ -22,14 +22,19 @@ const linksDef = [
   },
 ];
 const Header = ({ links = linksDef }) => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
+  const [trigger, setTrigger] = useState(false);
   const navigate = useNavigate();
   const anchorEl = useRef();
   const handleClick = () => {
     // e.preventDefault();
     setOpen(!open);
   };
-
+  useEffect(() => {
+    setIsAuth(AuthHelpers.isAuthenticated());
+  }, [trigger]);
+  console.log();
   // const open = Boolean(anchorEl);
   // const id = open ? "simple-popper" : undefined;
   return (
@@ -52,28 +57,31 @@ const Header = ({ links = linksDef }) => {
             </Link>
           ))}
         </span>
-        <Link
-          className="font-normal  flex items-center text-black  justify-end"
-          to={APP_ROUTES.HOME}
-        >
-          <Button className="py-1 px-4 border rounded-sm border-black border-1">
-            Login/Signup
-          </Button>
-        </Link>
-        {/* <span ref={anchorEl}>
-          <FaUserCircle
-            className="cursor-pointer h-8 w-8 transition text-green text-sm font-sans font-semibold leading-normal  text-white rounded-lg"
-            // aria-describedby={id}
-            type="button"
-            // onClick={handleClick}
-            onMouseEnter={handleClick}
-            onMouseLeave={
-              () => handleClick()
-              // setTimeout(() => {
-              // }, 2000)
-            }
-          />
-        </span> */}
+        {!isAuth ? (
+          <Link
+            className="font-normal  flex items-center text-black  justify-end"
+            to={APP_ROUTES.LOGIN}
+          >
+            <Button className="py-1 px-4 border rounded-sm border-black border-1">
+              Login/Signup
+            </Button>
+          </Link>
+        ) : (
+          <span ref={anchorEl}>
+            <FaUserCircle
+              className="cursor-pointer h-8 w-8 transition text-green text-sm font-sans font-semibold leading-normal  text-white rounded-lg"
+              // aria-describedby={id}
+              type="button"
+              // onClick={handleClick}
+              onMouseEnter={handleClick}
+              onMouseLeave={
+                () => handleClick()
+                // setTimeout(() => {
+                // }, 2000)
+              }
+            />
+          </span>
+        )}
         {/* <button
         >
           Toggle Popper
@@ -94,9 +102,10 @@ const Header = ({ links = linksDef }) => {
           <div
             onClick={() => {
               AuthHelpers.logout();
+              setTrigger(!trigger);
               navigate(APP_ROUTES.HOME);
             }}
-            className=" z-50 rounded-lg font-medium font-sans text-sm m-1 p-3 border border-solid border-slate-200 dark:border-slate-700 bg-white dark:bg-slate-900 shadow-md text-slate-900 dark:text-slate-100"
+            className=" z-50 rounded-lg cursor-pointer font-medium font-sans text-sm m-1 p-3 border border-solid border-slate-200 dark:border-slate-700 bg-white dark:bg-slate-900 shadow-md text-slate-900 dark:text-slate-100"
           >
             Logout
           </div>
