926ccefa00984cf25c662a3ab9aa719cfae09a22
ketan verma <ketan18710@gmail.com>
2024-03-18 22:00:56 +0530

    home page public

--- src/containers/App/routes.jsx
+++ src/containers/App/routes.jsx
@@ -24,7 +24,7 @@ import { toast } from "react-toastify";
 const allRoutes = [
   {
     path: APP_ROUTES.HOME,
-    isProtected: true,
+    isProtected: false,
     properties: { ketan: 1 },
     component: Home,
   },
